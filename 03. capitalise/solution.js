182ba258665704c0aecd43a5161c07719ff68b87
tarynJade <167077142+tarynJade@users.noreply.github.com>
2025-01-13 12:14:47 +0000

    tests passed for capitalise word

--- 03. capitalise/solution.js
+++ 03. capitalise/solution.js
@@ -1,8 +1,11 @@
 function capitalise(word) {
-  word[0] = word[0].toUpperCase();
-  return word;
+  const capitalisedFirstLetter = word[0].toUpperCase();
+  const restOfWord = word.slice(1).toLowerCase();
+  return capitalisedFirstLetter + restOfWord;
 }
 
 module.exports = { capitalise };
 
- 
+// capitalise first letter by using position 0 and toUpperCase()
+// Using slice method at position 1 to get the rest of string and ensuring it is set to lower case for any weird edge cases
+// return the whole word
